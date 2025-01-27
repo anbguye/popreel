30271cc05b4ba5ee2a7b91c6628f041da19c02e2
anbguye <anthonybnguyen56@gmail.com>
2025-01-26 18:13:51 -0600

    added some frontend

--- app/components/TikTokPost.tsx
+++ app/components/TikTokPost.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { Heart, MessageCircle, Bookmark, Share2, ChevronUp, ChevronDown } from "lucide-react"
+import { Heart, MessageCircle, Bookmark, Share2} from "lucide-react"
 import Image from "next/image"
 
 interface TikTokPostProps {
